787ed4aebeb0382b3fde3d1dbf65ff3db383aaca
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-03-09 21:58:40 +0300

    fisrt commit

--- main.sh
+++ main.sh
@@ -5,8 +5,8 @@ add-apt-repository ppa:pikaos/pika
 add-apt-repository ppa:kubuntu-ppa/backports
 add-apt-repository ppa:pikaos/ppa-deps
 # Clone Upstream
-git clone --recurse-submodules https://github.com/flightlessmango/MangoHud.git -b v0.6.8
-mv ./MangoHud ./mangohud
+tar -xf mangohud_0.6.8.orig.tar.xz -C ./
+mv ./MangoHud-v0.6.8./mangohud
 cp -rvf ./debian ./mangohud
 cd ./mangohud
 
@@ -14,7 +14,6 @@ cd ./mangohud
 apt-get build-dep ./ -y
 
 # Build package
-LOGNAME=root dh_make --createorig -y -l -p mangohud_0.6.8
 dpkg-buildpackage
 
 # Move the debs to output
--- main32.sh
+++ main32.sh
@@ -6,8 +6,8 @@ add-apt-repository ppa:pikaos/pika
 add-apt-repository ppa:kubuntu-ppa/backports
 add-apt-repository ppa:pikaos/ppa-deps
 # Clone Upstream
-git clone --recurse-submodules https://github.com/flightlessmango/MangoHud.git -b v0.6.8
-mv ./MangoHud ./mangohud
+tar -xf mangohud_0.6.8.orig.tar.xz -C ./
+mv ./MangoHud-v0.6.8./mangohud
 cp -rvf ./debian ./mangohud
 cd ./mangohud
 
