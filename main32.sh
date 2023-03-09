e8c73767a2ec246816a5e305ff9425b9ad3f5ea0
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-03-09 21:29:53 +0300

    fisrt commit

---
+++ main32.sh
@@ -0,0 +1,25 @@
+# Add dependent repositories
+sudo dpkg --add-architecture i386
+wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
+add-apt-repository https://ppa.pika-os.com
+add-apt-repository ppa:pikaos/pika
+add-apt-repository ppa:kubuntu-ppa/backports
+# Clone Upstream
+git clone https://github.com/flightlessmango/MangoHud.git -b v0.6.8
+mv ./MangoHud ./mangohud
+cp -rvf ./debian ./mangohud
+cd ./mangohud
+
+# Get build deps
+apt-get install build-essential -y
+apt-get install crossbuild-essential-i386 lib32gcc-11-dev -y
+apt-get build-dep ./ -y -a i386
+
+# Build package
+dpkg-buildpackage -a i386
+
+# Move the debs to output
+cd ../
+mkdir -p ./output
+mv ./*.deb ./output/
+
