e5b5b957afb6e0a2c7a48845fa1d114fad810b12
ferrreo <harderthanfire@gmail.com>
2023-06-16 19:21:01 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,3 @@
-# Add dependent repositories
-wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-touch /etc/apt/sources.list.d/pika.list
-echo 'deb https://ppa.pika-os.com/ lunar main' > /etc/apt/sources.list.d/pika.list
-add-apt-repository ppa:pikaos/pika
-add-apt-repository ppa:kubuntu-ppa/backports
-apt update
 # Clone Upstream
 tar -xf mangohud_0.6.8.orig.tar.xz -C ./
 mv ./MangoHud-v0.6.8 ./mangohud
